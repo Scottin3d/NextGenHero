408ffd3f82e0171554d92e39227c80654060119a
Scott <scottin3d@gmail.com>
2020-05-01 08:32:05 -0700

    working waypoint spawns

--- Assets/Scripts/SpawnPoints.cs
+++ Assets/Scripts/SpawnPoints.cs
@@ -5,17 +5,15 @@ using UnityEngine;
 public class SpawnPoints : MonoBehaviour {
   [SerializeField]
   private int numberOfSpawns = 10;
+  [SerializeField]
   Vector3[] spawnPoints;
   bool[] spawnFilled;
+
   // Start is called before the first frame update
   void Start() {
     spawnPoints = new Vector3[numberOfSpawns];
     spawnFilled = new bool[numberOfSpawns];
-  }
-
-  // Update is called once per frame
-  void Update() {
-
+    InitializeSpawns(numberOfSpawns);
   }
 
   void InitializeSpawns(int numOfSpawns) {
@@ -25,14 +23,22 @@ public class SpawnPoints : MonoBehaviour {
     }
   }
 
-
   Vector3 GenerateSpawn() {
     float spawnY = Random.Range(Camera.main.ScreenToWorldPoint(new Vector2(0, 0)).y, 
                                 Camera.main.ScreenToWorldPoint(new Vector2(0, Screen.height)).y);
     float spawnX = Random.Range(Camera.main.ScreenToWorldPoint(new Vector2(0, 0)).x, 
                                 Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, 0)).x);
-
+  
     Vector3 spawnPosition = new Vector3(spawnX, spawnY, 0);
     return spawnPosition;
+  }
+
+  public Vector3 GetRNGWaypointSpawn() {
+    int rng = Random.Range(0, numberOfSpawns -1);
+    while (spawnFilled[rng]) {
+      rng = Random.Range(0, numberOfSpawns - 1);
+    }
+    spawnFilled[rng] = true;
+    return spawnPoints[rng];
   }
 }
---
+++ Assets/Waypoints.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waypoints : MonoBehaviour {
+  public Sprite[] sprites;
+
+  [SerializeField]
+  SpawnPoints spawnPoints;
+  [SerializeField]
+  GameObject waypointPrefab;
+
+  int numberOfWaypoints = 6;
+  [SerializeField]
+  List<GameObject> waypoints;
+
+  // Start is called before the first frame update
+  void Start() {
+    waypoints = new List<GameObject>();
+    spawnPoints = GameObject.Find("SpawnPoints").GetComponent<SpawnPoints>();
+    waypointPrefab = GameObject.Find("WaypointPrefab");
+    InitializeWaypoints();
+  }
+
+  // initialize waypoints
+  void InitializeWaypoints() {
+    for (int i = 0; i < numberOfWaypoints; i++) {
+      GenerateWaypoint(sprites[i]);
+    }
+  }
+
+  // generate waypoint
+  void GenerateWaypoint(Sprite sprite) {
+    Vector3 spawnPos = spawnPoints.GetRNGWaypointSpawn();
+    GameObject waypoint = Instantiate(waypointPrefab);
+    waypoint.transform.position = spawnPos;
+    waypoint.GetComponent<SpriteRenderer>().sprite = sprite;
+
+    waypoints.Add(waypoint);
+  }
+
+}
